5a18a65f8e661ffb766cf429f5d032a754cc2ce3
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-02-02 09:07:11 +0200

    Add tasjk 3

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -11,6 +11,13 @@ function App() {
             </div>
             <div className="App-body">
                 <p>Login to access the full dashboard</p>
+                <form action="">
+                    <label htmlFor="email">Email: </label>
+                    <input type="email" id="email" name="email" />
+                    <label htmlFor="password">Password: </label>
+                    <input type="password" id="password" name="password" />
+                    <button type="submit">OK</button>
+                </form>
             </div>
             <div className="App-footer">
                 <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
