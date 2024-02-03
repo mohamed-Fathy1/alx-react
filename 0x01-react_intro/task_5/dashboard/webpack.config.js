bcdde5c003d96dc97a4264b3aa8533895c36b769
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-02-03 09:53:22 +0200

    Add task 9

---
+++ 0x01-react_intro/task_5/dashboard/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    target: 'web',
+    devServer: {
+        port: 3000,
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        open: true,
+        hot: true,
+        liveReload: true,
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react', '@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'images',
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './public/index.html',
+        }),
+    ],
+};
