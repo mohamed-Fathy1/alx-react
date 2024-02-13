653f88915ca2d3b431e91148854a05c3c5c19e34
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-02-13 10:57:26 +0200

    Add task 1

--- 0x02-react_props/task_0/dashboard/src/App/App.test.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.test.js
@@ -5,6 +5,26 @@ import App from './App';
 describe('Test App Compunent', () => {
     it('renders without crashing', () => {
         const wrapper = shallow(<App />);
-        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.exists()).toBe(true);
+    });
+
+    it('renders a header', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('Header').length).toBe(1);
+    });
+
+    it('renders a Login component', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('Login').length).toBe(1);
+    });
+
+    it('renders a Footer component', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('Footer').length).toBe(1);
+    });
+
+    it('renders a Notification component', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('Notification').length).toBe(1);
     });
 });
