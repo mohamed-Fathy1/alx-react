b0fd558f726529695dd68347756aa761005db5fa
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-02-16 14:06:13 +0200

    Add task 0

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -8,30 +8,32 @@ import { getLatestNotification } from "../utils/utils";
 import "./App.css";
 import PropTypes from 'prop-types';
 
-function App({ isLoggedIn }) {
+class App extends React.Component {
+    render() {
 
-    const listCourses = [
-        { id: 1, name: 'ES6', credit: 60 },
-        { id: 2, name: 'Webpack', credit: 20 },
-        { id: 3, name: 'React', credit: 40 }
-    ];
+        const listCourses = [
+            { id: 1, name: 'ES6', credit: 60 },
+            { id: 2, name: 'Webpack', credit: 20 },
+            { id: 3, name: 'React', credit: 40 }
+        ];
 
-    const  listNotifications = [
-        { id: 1, type: 'default', value: 'New course available' },
-        { id: 2, type: 'urgent', value: 'New resume available' },
-        { id: 3, type: 'urgent', html: { __html: getLatestNotification() } }
-    ];
+        const listNotifications = [
+            { id: 1, type: 'default', value: 'New course available' },
+            { id: 2, type: 'urgent', value: 'New resume available' },
+            { id: 3, type: 'urgent', html: { __html: getLatestNotification() } }
+        ];
 
-    return (
-        <>
-            <Notifications displayDrawer={true} listNotifications={listNotifications} />
-            <div className="App">
-                <Header />
-                {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
-                <Footer />
-            </div>
-        </>
-    );
+        return (
+            <>
+                <Notifications displayDrawer={true} listNotifications={listNotifications} />
+                <div className="App">
+                    <Header />
+                    {this.props.isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+                    <Footer />
+                </div>
+            </>
+        );
+    }
 }
 
 App.propTypes = {
