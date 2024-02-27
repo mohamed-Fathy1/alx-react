af93777849bb1643cc675e2808b66121124b43a2
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-02-27 04:34:20 +0200

    Add task 10

--- 0x03-React_component/task_5/dashboard/src/Notifications/Notifications.js
+++ 0x03-React_component/task_5/dashboard/src/Notifications/Notifications.js
@@ -14,6 +14,10 @@ class Notifications extends React.Component {
         this.markAsRead = this.markAsRead.bind(this);
     }
 
+    shouldComponentUpdate(nextProps) {
+        return nextProps.listNotifications.length > this.props.listNotifications.length;
+    }
+
     markAsRead(id) {
         console.log(`Notification ${id} has been marked as read`);
     }
