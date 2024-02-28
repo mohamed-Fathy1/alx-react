2fd642371441ad0cde8d85ff572380e8c592e8cb
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-02-28 06:27:46 +0200

    Add task 1

--- 0x04-React_inline_styling/task_1/dashboard/src/Login/Login.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Login/Login.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import './Login.css';
 import withLogging from '../HOC/WithLogging';
+import { StyleSheet, css } from 'aphrodite';
+
+
+const styles = StyleSheet.create({
+
+    AppBody: {
+        marginTop: '1rem',
+        padding: 'calc(2rem + 2vw)',
+        minHeight: '60vh',
+        '@media (max-width: 768px)': {
+            height: '40vh'
+        }
+    }
+
+})
 
 function Login() {
     return (
         <>
-            <div className="App-body">
+            <div className={css(styles.AppBody)}>
                 <p>Login to access the full dashboard</p>
                 <form action="" style={{ marginTop: '1rem' }}>
                     <label htmlFor="email">Email: </label>
