040b7ec11abd208f7c21ab84b527475e5d44328a
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-02-28 06:20:51 +0200

    Add taask 1

--- 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import './Header.css';
 import logo from '../assets/holberton-logo.jpg';
+import { StyleSheet, css } from 'aphrodite';
+
+const mobile = '@media(max-width: 768px)';
+
+const styles = StyleSheet.create({
+    appHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        borderBottom: '4px solid var(--color-primary)',
+        [mobile]: {
+            flexDirection: 'column',
+        }
+    },
+    headerImage: {
+        height: '12rem',
+        [mobile]: {
+            height: '8rem',
+        }
+    },
+    headerTitle: {
+        marginLeft: '1rem',
+        color: 'var(--color-primary)',
+        [mobile]: {
+            marginLeft: 0,
+            marginTop: '1rem',
+        }
+    }
+});
+
 
 function Header() {
     return (
-        <header className="App-header">
-            <img src={logo} alt="Holberton Logo" />
-            <h1>School dashboard</h1>
+        <header className={css(styles.appHeader)}>
+            <img src={logo} alt="Holberton Logo" className={css(styles.headerImage)} />
+            <h1 className={css(styles.headerTitle)}>School dashboard</h1>
         </header>
     );
 }
