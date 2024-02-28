42b68fcc61dbbb9312a09128cd2e9f9e790f9787
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-02-28 05:55:09 +0200

    Add task 1

--- 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseList.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseList.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import CourseListRow from './CourseListRow';
-import './CourseList.css';
 import PropTypes from 'prop-types';
 import CourseShape from './CourseShape';
+import { StyleSheet, css } from 'aphrodite';
 
+
+const styles = StyleSheet.create({
+    appBody: {
+        padding: 'calc(2rem + 2vw)',
+        minHeight: '60vh',
+        '@media (max-width: 768px)': {
+            height: '40vh',
+        },
+    },
+    table: {
+        width: '100%',
+        maxWidth: '60rem',
+        borderCollapse: 'collapse',
+        margin: '0 auto',
+        marginTop: '2rem',
+        border: '1px solid gray',
+    },
+    tableRowWithSecondChild: {
+        '&:nth-child(2) td': {
+            textAlign: 'left',
+        },
+    },
+    tableCell: {
+        padding: '5px',
+        border: '1px solid gray',
+    },
+    tableHeader: {
+        border: '1px solid gray',
+    },
+});
 const CourseList = ({ listCourses }) => {
     return (
-        <div className="App-body">
-            <table id='CourseList'>
+        <div className={css(styles.appBody)}>
+            <table id='CourseList' className={css(styles.table)}>
                 <thead>
                     <CourseListRow isHeader={true} textFirstCell="Available courses" />
                     <CourseListRow isHeader={true} textFirstCell="Course name" textSecondCell="Credit" />
--- 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseListRow.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import './CourseListRow.css'
 
 const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
-    const styles = {
-        row: {
-            backgroundColor: isHeader ? '#deb5b545' : '#f5f5f5ab'
-        }
+    const row = {
+        backgroundColor: isHeader ? '#deb5b545' : '#f5f5f5ab'
     }
+
     return (
-        <tr style={styles.row}>
+        <tr style={row}>
             {isHeader ? (
                 textSecondCell === null ? (
                     <th colSpan="2">{textFirstCell}</th>
