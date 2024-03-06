ba91a9061b7cff65681006f2f7539a0e861dc52f
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-03-06 06:06:04 +0200

    Add task 0

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,5 +1,15 @@
-import { fromJS } from 'immutable';
+#!/usr/bin/node
+const Immutable = require('immutable');
 
-export default function getImmutableObject(object) {
-  return fromJS(object);
+function getImmutableObject(object) {
+    return Immutable.fromJS(object)
 }
+
+const x = {
+     fear: true,
+     smell: -1033575916.9145899,
+     wall: false,
+     thing: -914767132
+}
+
+console.log(getImmutableObject(x))
