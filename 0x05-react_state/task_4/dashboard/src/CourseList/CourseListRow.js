f1b680cdcd0584ee77d79ebf36507b3afdbafc54
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-03-06 03:50:34 +0200

    Add task 4

--- 0x05-react_state/task_4/dashboard/src/CourseList/CourseListRow.js
+++ 0x05-react_state/task_4/dashboard/src/CourseList/CourseListRow.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet, css } from 'aphrodite';
 
@@ -7,7 +7,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#deb5b545'
     },
     defaultRow: {
-        backgroundColor:  '#f5f5f5ab'
+        backgroundColor: '#f5f5f5ab'
     },
     data: {
         padding: '5px'
@@ -17,13 +17,18 @@ const styles = StyleSheet.create({
     },
     textLeft: {
         textAlign: 'left'
+    },
+    rowChecked: {
+        backgroundColor: '#e6e4e4'
     }
 })
 
 const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
 
+    const [isChecked, setChecked] = useState(false)
+
     return (
-        <tr className={css(isHeader? styles.headerRow : styles.defaultRow)}>
+        <tr className={css(isHeader ? styles.headerRow : isChecked ? styles.rowChecked : styles.defaultRow)}>
             {isHeader ? (
                 textSecondCell === null ? (
                     <th colSpan="2" className={css(styles.th, styles.data)}>{textFirstCell}</th>
@@ -34,7 +39,10 @@ const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
                     </>
                 )) : (
                 <>
-                    <td className={css(styles.data)}>{textFirstCell}</td>
+                    <td className={css(styles.data)}>
+                        <input onClick={() => setChecked(prev => !prev)} type="checkbox" />
+                        {textFirstCell}
+                    </td>
                     <td className={css(styles.data)}>{textSecondCell}</td>
                 </>
             )}
