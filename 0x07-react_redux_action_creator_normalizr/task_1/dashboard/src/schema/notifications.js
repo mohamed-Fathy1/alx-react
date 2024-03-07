7857b4196624b101565248f583b0b27f979f0c39
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-03-07 15:12:19 +0200

    Add task 1

--- 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -1,4 +1,5 @@
 import * as notificationsData from '../../notifications.json'
+import { normalize, schema } from 'normalizr'
 
 
 function getAllNotificationsByUser(userId) {
@@ -7,4 +8,18 @@ function getAllNotificationsByUser(userId) {
         .map((notification) => notification.context)
 }
 
-export default getAllNotificationsByUser
+const user = new schema.Entity('users')
+const message = new schema.Entity('messages', {}, {
+    idAttribute: 'guid'
+})
+const notification = new schema.Entity('notifications', {
+    author: user,
+    context: message
+})
+
+const mySchema = [notification]
+
+const normalizedData = normalize(notificationsData.default, mySchema)
+
+
+export { getAllNotificationsByUser, normalizedData }
